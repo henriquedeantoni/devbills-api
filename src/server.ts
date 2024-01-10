f123c838cd9b110da524b13cd56fd667d0a5e51b
Henrique De Antoni <147777120+henriquedeantoni@users.noreply.github.com>
2024-01-10 20:09:10 -0300

    Create server.ts

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import express, { json } from 'express';
+
+import { setupMongo } from './database';
+import { errorHandler } from './middlewares/error-handler.middleware';
+import { routes } from './routes';
+
+setupMongo().then(() => {
+  const app = express();
+
+  app.use(json());
+  app.use(routes);
+  app.use(errorHandler);
+
+  app.listen(3333, () => console.log('🚀 App is running at port 3333!'));
+});
