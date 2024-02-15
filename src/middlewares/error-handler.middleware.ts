9ca84d6800b388d7fd1a2e051aea22f05be47f84
Henrique De Antoni <147777120+henriquedeantoni@users.noreply.github.com>
2024-02-15 09:36:41 -0300

    Create error-handler.middleware.ts

---
+++ src/middlewares/error-handler.middleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import { AppError } from '../errors/app.error';
+
+export function errorHandler(
+  error: AppError | Error,
+  _: Request,
+  res: Response,
+  __: NextFunction,
+) {
+  if (error instanceof AppError) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    message: error.message,
+  });
+}
